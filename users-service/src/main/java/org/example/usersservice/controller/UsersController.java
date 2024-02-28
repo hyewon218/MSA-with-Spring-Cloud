e4eb34b99dedfe191ffbef097e9d4801eaa77624
hyewon218 <won_11111@naver.com>
2024-02-28 21:02:46 +0900

    Section 4: Eureka 클라이언트 자격 등록 및 yml 설정

---
+++ users-service/src/main/java/org/example/usersservice/controller/UsersController.java
@@ -0,0 +1,15 @@
+package org.example.usersservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UsersController {
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+}
