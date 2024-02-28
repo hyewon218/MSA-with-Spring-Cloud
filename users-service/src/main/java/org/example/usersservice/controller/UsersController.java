759f08e9ce4c9944cbe4b4b14aa638307ff13018
hyewon218 <won_11111@naver.com>
2024-02-28 21:18:44 +0900

    Section 4: Users Microservice - welcome() 메소드 Environment 사용

--- users-service/src/main/java/org/example/usersservice/controller/UsersController.java
+++ users-service/src/main/java/org/example/usersservice/controller/UsersController.java
@@ -1,5 +1,7 @@
 package org.example.usersservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class UsersController {
 
+    private Environment env;
+
+    @Autowired
+    public UsersController(Environment env) { // 생성자 주입
+        this.env = env;
+    }
+
     @GetMapping("/health_check")
     public String status() {
         return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return env.getProperty("greeting.message");
     }
 }
