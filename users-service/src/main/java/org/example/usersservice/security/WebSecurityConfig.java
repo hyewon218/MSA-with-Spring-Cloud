42b7e6be08340fccd9877026b5c569c597972cf4
hyewon218 <won_11111@naver.com>
2024-02-29 17:10:33 +0900

    Section 4: Users Microservice - Spring Security 연동

---
+++ users-service/src/main/java/org/example/usersservice/security/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package org.example.usersservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration// 다른 빈보다 등록 우선순위가 높아진다.
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.csrf( (csrf) -> csrf.disable());
+//      http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests((authz) -> authz
+                    .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
+//                  .requestMatchers("/**").access(this::hasIpAddress)
+                    .requestMatchers("/**").access(
+                        new WebExpressionAuthorizationManager("hasIpAddress('127.0.0.1')"))
+                    .anyRequest().authenticated()
+            )
+            .sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.headers((headers) -> headers.frameOptions(FrameOptionsConfig::sameOrigin));// h2-console 에 접근하기 위함
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
