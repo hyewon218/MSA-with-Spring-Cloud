1942af8c27508d4d26650f07933d89181a4e39ac
hyewon218 <won_11111@naver.com>
2024-03-04 23:56:48 +0900

    Section 6: Users Microservice - apigateway-service Routes 정보 변경

--- users-service/src/main/java/org/example/usersservice/controller/UsersController.java
+++ users-service/src/main/java/org/example/usersservice/controller/UsersController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users-service")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class UsersController {
 
