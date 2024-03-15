97f74b5d92ef630082bc362108638ea70bd8d20a
hyewon218 <won_11111@naver.com>
2024-03-15 23:20:44 +0900

    Section 10: Microservcie 간 통신 - 주소값 대신 Eureka 서버에 등록된 Microservcie name 으로 대체

--- orders-service/src/main/java/org/example/ordersservice/OrdersServiceApplication.java
+++ orders-service/src/main/java/org/example/ordersservice/OrdersServiceApplication.java
@@ -2,7 +2,9 @@ package org.example.ordersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class OrdersServiceApplication {
 
